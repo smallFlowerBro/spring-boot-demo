48f6df571af443869172f1004d887759b3f78baf
weiyongjian <1262405508@qq.com>
2021-08-23 14:47:41 +0800

    #swagger 接口测试

---
+++ spring-boot-swagger/src/main/java/com/wei/swagger/config/SwaggerConfig.java
@@ -0,0 +1,48 @@
+package com.wei.swagger.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @Author weiyongjian
+ * @Description //TODO
+ * @Date
+ */
+@Configuration
+@ConfigurationProperties(value = "spring.swagger")
+@EnableSwagger2
+@Data
+public class SwaggerConfig {
+
+    private String packages;
+
+    @Bean
+    public Docket getDocket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(this.getApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(this.packages))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo getApiInfo(){
+        return new ApiInfoBuilder()
+                .title("swagger demo")
+                .description("更多详情")
+                .version("1.0")
+                .build();
+    }
+
+
+
+}
---
+++ spring-boot-swagger/src/main/java/com/wei/swagger/controller/HelloController.java
@@ -0,0 +1,30 @@
+package com.wei.swagger.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author weiyongjian
+ * @Description //TODO
+ * @Date
+ */
+
+@RestController
+@RequestMapping("/hello")
+@Api(description = "测试管理")
+public class HelloController {
+
+    @GetMapping("world")
+    @ApiOperation(value = "测试接口一" ,notes = "这是一个测试接口")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "name", value = "姓名",defaultValue = "小花" )
+    })
+    public String  world(){
+        return "hello world!";
+    }
+}
