73a5b85f3075354562906ef55f8c2d2a2696177e
weiyongjian <1262405508@qq.com>
2021-09-13 13:34:00 +0800

    ## 调整log4j2.yml

--- spring-boot-log-log4j2/src/test/java/com/wei/log4j2/SpringBootLogLog4j2ApplicationTests.java
+++ spring-boot-log-log4j2/src/test/java/com/wei/log4j2/SpringBootLogLog4j2ApplicationTests.java
@@ -1,19 +1,22 @@
 package com.wei.log4j2;
 
+
+
+
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
+import org.slf4j.MDC;
+
 import org.springframework.boot.test.context.SpringBootTest;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @SpringBootTest
 @Log4j2
 class SpringBootLogLog4j2ApplicationTests {
-
     @Test
     void contextLoads() {
-        System.setProperty("log.dir","ZZJ01");
-        log.error("dit it again!");
+//        System.setProperty("logPath","ZZJ01");
+        //MDC.put("deviceNo","ZZJ01");
+        log.error("测试");
     }
 
 }
