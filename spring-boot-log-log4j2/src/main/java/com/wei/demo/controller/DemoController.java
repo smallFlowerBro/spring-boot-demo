1da72f5fc174aa505acf7c94c5869d547ba96a87
weiyongjian <1262405508@qq.com>
2023-02-27 22:03:25 +0800

    ## log4j2

--- spring-boot-log-log4j2/src/main/java/com/wei/demo/controller/DemoController.java
+++ spring-boot-log-log4j2/src/main/java/com/wei/demo/controller/DemoController.java
@@ -1,9 +1,13 @@
 package com.wei.demo.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.ThreadContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 /*********************************
  *   @Author WEIYJ
@@ -16,7 +20,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     @GetMapping("hello")
-    private String hello(){
+    private String hello(@RequestParam String deviceNo){
+
+//        MDC.put("deviceNo",deviceNo);
+        ThreadContext.put("deviceNo",deviceNo);
         log.info("进入hello");
         return "hello";
     }
